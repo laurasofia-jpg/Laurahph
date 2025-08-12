c308fd137811c584c3a54f3eaf77815ed63db381
laurasofia-jpg <lulitogarzon123@gmail.com>
2025-08-12 17:31:45 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,11 @@
+<?php
+$Identificacion= $_POST ['iDENTIFICACION'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST ['Cargo'];
+$Salario=$_POST [ 'Salario'];
+echo "La identificacion del Empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleado es: ".Nomapellidos;
+echo "El cargo del empleado es: ".Cargo;
+echo "El salario del empleado es: ".Salario;
+?>
 
